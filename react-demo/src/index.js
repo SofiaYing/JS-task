80c58e78ec5921359dd831076841a0d690a05fbb
sofia <zhangyySmile@126.com>
2018-06-04 15:22:42 +0800

    modify

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
+  <h1>Hello, world!</h1>,
   document.getElementById('root')
 );
