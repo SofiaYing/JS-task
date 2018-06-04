f7948537cbfbac7386d4fa0f697ba8bad94bdbac
sofia <zhangyySmile@126.com>
2018-06-04 15:07:40 +0800

    react build

---
+++ react-demo/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+ReactDOM.render(
+  <h1>Hi, world!</h1>,
+  document.getElementById('root')
+);
